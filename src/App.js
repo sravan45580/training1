b72ae7e315a5b89f8c8490fba31749c7cf36e60e
sravan <vorugantisravan8@gmail.com>
2025-02-05 12:51:06 +0530

    push

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Main from './components/Main'
-import Sidebar1 from './components/Sidebar1'
-import Sidebar2 from './components/Sidebar2'
-import Footer from './components/Footer'
-const App = () => {
-return (
-<div className='app'>
-  <Navbar/>
-  <Main/>
-  <div className='sidebar'>
-    <Sidebar1/>
-    <Sidebar2/>
-    </div>
-    <Footer/>
-    </div>
- )
-}
-export default App
+// import React from 'react'
+// import Navbar from './components/Navbar'
+// import Main from './components/Main'
+// import Sidebar1 from './components/Sidebar1'
+// import Sidebar2 from './components/Sidebar2'
+// import Footer from './components/Footer'
+// const App = () => {
+// return (
+// <div className='app'>
+//   <Navbar/>
+//   <Main/>
+//   <div className='sidebar'>
+//     <Sidebar1/>
+//     <Sidebar2/>
+//     </div>
+//     <Footer/>
+//     </div>
+//  )
+// }
+// export default App
+
+
+import React, { Component, startTransition } from 'react'
+import CBCPropEx from './propsex/CBCPropEx'
+export default class App extends Component {
+    render() {
+        return (
+        <div>
+            /*<CBCPropEx
+            username="Vamshi"
+            age={20}
+            hobbies={["Playing", "reading books" , "stock"]}
+            address={{city: "sircilla", area: "Gandinagar"}}
+            sendFun={function(){alert("hi i am vamshi")}}*/
+            />
+</div>
+        )
+    }
+  }
